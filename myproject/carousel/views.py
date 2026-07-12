eaae3608919d10a11fefd7f8dceebf1f29366e4f
renukasirolu <renukasirolu055@gmail.com>
2026-07-12 19:33:47 +0530

    Add files via upload

---
+++ myproject/carousel/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from .models import CarouselImage
+def carousel_view(request):
+images = CarouselImage.objects.all()
+return render(request, 'carousel/carousel.html', {'images': images})
